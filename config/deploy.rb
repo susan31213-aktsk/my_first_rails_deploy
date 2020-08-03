54f3249329216f3b486601a38c7d54ecdae112d0
susan31213-aktsk <68223819+susan31213-aktsk@users.noreply.github.com>
2020-08-03 14:37:03 +0800

    new credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "my_first_deploy"
+set :repo_url, "git@github.com:susan31213-aktsk/my_first_rails_deploy.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set :deploy_to, "/home/deploy/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -37,3 +37,11 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Default value for :linked_files is []
+append :linked_files, "config/master.key"
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules', 'public/shared', 'public/uploads'
+
+# Default value for default_env is {}
+set :default_env, { path: '/usr/local/ruby-2.7.1/bin:$PATH' }
